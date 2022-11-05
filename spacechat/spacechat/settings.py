5148b49626d7503d0e08c0beff3ed4a5b8d94c86
Pavel Loginov <pavel.loginov.dev@gmail.com>
2022-11-05 19:56:21 +0300

    Added lost data.

--- spacechat/spacechat/settings.py
+++ spacechat/spacechat/settings.py
@@ -31,7 +31,7 @@ PROJECT_DIR = BASE_DIR.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
