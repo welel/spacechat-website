8b97e89249d1d7b6969bf8dce28e6aaaf8144ae5
Pavel Loginov <pavel.loginov.dev@gmail.com>
2022-11-05 19:34:50 +0300

    Init.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# spacechat-website
+Starts from a course: "Learn Django - Build an Asynchronous Chatroom with Django and Channels"
+https://www.youtube.com/watch?v=F4nwRQPXD8w
---
+++ spacechat/spacechat/asgi.py
@@ -0,0 +1,30 @@
+"""
+ASGI config for spacechat project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+import chat.routing
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spacechat.settings')
+
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    'http': django_asgi_app,
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
